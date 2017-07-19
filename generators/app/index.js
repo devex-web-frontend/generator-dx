aaad3f028ba8fc956b1f155ec5ce151f22568111
Kirill Agalakov <agalakov@devexperts.com>
2017-07-19 13:30:03 +0300

    cra eject

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,9 +1,5 @@
 const Generator = require('yeoman-generator');
 const path = require('path');
-const glob = require('glob');
-
-const ROOT = path.resolve(__dirname);
-const cwd = process.cwd();
 
 module.exports = class extends Generator {
 	constructor(args, opts) {
@@ -23,14 +19,6 @@ module.exports = class extends Generator {
 	writing() {
 		const dest = this.destinationRoot();
 		const source = this.sourceRoot();
-		this.log(source);
-		const files = glob.sync(path.join(source, '**/*.*'), {
-			dot: true
-		});
-		// files.forEach(file => {
-		// 	this.fs.copy(file, dest);
-		// });
-		// this.log(files);
 		const context = {
 			name: this.options.name
 		};
@@ -41,42 +29,3 @@ module.exports = class extends Generator {
 		});
 	}
 };
-
-// 'use strict';
-//
-// var yeoman = require('yeoman-generator');
-// var chalk = require('chalk');
-// var yosay = require('yosay');
-//
-// var CREATE_ENTITY = {
-// 	value: 'createEntity',
-// 	name: 'Create one of project entity'
-// };
-//
-// module.exports = yeoman.generators.Base.extend({
-// 	prompting: function () {
-// 		var done = this.async();
-// 		var prompts = [
-// 			{
-// 				type: 'list',
-// 				name: 'action',
-// 				message: 'What you want to do?',
-// 				choices: [CREATE_ENTITY],
-// 				default: CREATE_ENTITY.value
-// 			}
-// 		];
-//
-// 		this.log(require('../logo.js'));
-//
-// 		this.prompt(prompts, function(props) {
-// 			this.props = props;
-// 			done();
-// 		}.bind(this));
-// 	},
-// 	end: function() {
-// 		if (this.props.action === CREATE_ENTITY.value) {
-// 			this.composeWith('dx:entity');
-// 		}
-// 	}
-// });
-//
